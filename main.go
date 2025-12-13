a27505d546a693646cc6d2ebfdc305ca29c27ee5
Zoe <caramel.moustache@orange.fr>
2025-12-13 13:21:43 +0100

    commands logic done

--- main.go
+++ main.go
@@ -3,9 +3,61 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Omorfii/aggregator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name      string
+	arguments []string
+}
+
+func handlerLogin(s *state, cmd command) error {
+
+	if len(cmd.arguments) <= 0 {
+		return fmt.Errorf("No username given")
+	}
+
+	firstArgument := cmd.arguments[0]
+
+	err := s.cfg.SetUser(firstArgument)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("User has been set")
+
+	return nil
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+
+	function, exists := c.handlers[cmd.name]
+	if exists {
+		return function(s, cmd)
+	} else {
+		return fmt.Errorf("Command not found")
+	}
+
+}
+
+func (c *commands) register(name string, f func(*state, command) error) error {
+
+	if _, exists := c.handlers[name]; exists {
+		return fmt.Errorf("handler for command %s already exists", name)
+	}
+	c.handlers[name] = f
+	return nil
+}
 
 func main() {
 
@@ -14,15 +66,31 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = cfg.SetUser("Zoe")
-	if err != nil {
-		log.Fatal(err)
+	currentConfig := state{
+		cfg: &cfg,
 	}
 
-	updatedCfg, err := config.Read()
-	if err != nil {
-		log.Fatal(err)
+	currentCommands := commands{
+		handlers: make(map[string]func(*state, command) error),
 	}
 
-	fmt.Println(updatedCfg)
+	currentCommands.register("login", handlerLogin)
+
+	arguments := os.Args
+
+	if len(arguments) < 2 {
+		fmt.Println("not enough argument")
+		os.Exit(1)
+	}
+
+	userCommand := command{
+		name:      arguments[1],
+		arguments: arguments[2:],
+	}
+
+	if err := currentCommands.run(&currentConfig, userCommand); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 }
